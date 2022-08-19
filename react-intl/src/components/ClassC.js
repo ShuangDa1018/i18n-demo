7dae96241465465926d3ced3ed604cc4d96e8706
tanshuang <763943829@qq.com>
2022-08-19 16:55:09 +0800

    react码

--- react-intl/src/components/ClassC.js
+++ react-intl/src/components/ClassC.js
@@ -1,12 +1,24 @@
-import { FormattedMessage ,injectIntl} from 'react-intl'
+import { FormattedMessage, injectIntl } from 'react-intl'
+import { intl } from '../i18n.js'
 import React from 'react'
 class classC extends React.Component {
+     changeLanguage  () {
+        let lang = intl.locale
+        lang = lang === 'zh' ? 'en' : 'zh'
+        intl.locale = lang
+        console.log(intl)
+        // this.setState({ locale: lang });
+      }
   render() {
-    return<div>
-      <p>
-        <FormattedMessage id="help" />
-      </p>
-    </div>
+    return (
+      <div>
+      <div onClick={this.changeLanguage}>点击切换语言</div>
+        <p>
+          <FormattedMessage id="help" />
+      {this.props.intl.formatMessage({id:'personalCenter'})}
+        </p>
+      </div>
+    )
   }
 }
 export default injectIntl(classC)
--- react-intl/src/components/hooksC.js
+++ react-intl/src/components/hooksC.js
@@ -1,15 +1,21 @@
 // import { useIntl } from 'react-intl'
 import { intl } from '../i18n.js'
+
 function CreateSqlTable(props) {
-//   const intl = useIntl()
+  //   const intl = useIntl()
+  const changeLanguage = () => {
+    let lang = intl.locale
+    lang = lang === 'zh' ? 'en' : 'zh'
+    intl.locale = lang
+    console.log(intl)
+    this.setState({ locale: lang });
+  }
   return (
     <div>
-        <input placeholder={intl.formatMessage({id: 'help'})}
-    />
-    {intl.formatMessage({id:'logout'})}
-        {/* {this.props.intl.formatMessage({id:'help'})} */}
+      {/* <div onClick={changeLanguage}>点击切换语言</div> */}
+      <input placeholder={intl.formatMessage({ id: 'help' })} />
+      {intl.formatMessage({ id: 'logout' })}
     </div>
-    
   )
 }
 
